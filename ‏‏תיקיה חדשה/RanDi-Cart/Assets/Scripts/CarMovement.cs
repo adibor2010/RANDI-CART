7946f4dae131e467037ebebd4127ae7c964d67ae
adibor2010 <adibor300610@gmail.com>
2023-08-30 00:37:59 +0300

    WE DID TAHAT

--- ‏‏תיקיה חדשה/RanDi-Cart/Assets/Scripts/CarMovement.cs
+++ ‏‏תיקיה חדשה/RanDi-Cart/Assets/Scripts/CarMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,17 @@ public class CarMovement : MonoBehaviour
     [SerializeField] float moveSpeed = 15f;
     [SerializeField] float rotateSpeed = 225f;
 
+    [SerializeField] float stunnedTimer = 0f;
+    [SerializeField] float stunnedTimeRate = 0.5f;
+
     float moveAmount;
     float rotateAmount;
 
     [SerializeField] int playerNum;
 
     Rigidbody2D rb;
+
+    bool isStunned = false;
 
     private void Start()
     {
@@ -21,25 +27,51 @@ public class CarMovement : MonoBehaviour
 
     private void Update()
     {
+        if (isStunned == true)
+        {
+            StartStunnTimer();
+        }
         moveAmount = moveSpeed * Input.GetAxis($"{playerNum}Vertical");
 
         rotateAmount = -rotateSpeed * Input.GetAxis($"{playerNum}Horizontal");
     }
 
+    private void StartStunnTimer()
+    {
+        if (stunnedTimer < stunnedTimeRate)
+        {
+            stunnedTimer += Time.deltaTime;
+            if (stunnedTimer >= stunnedTimeRate)
+            {
+                isStunned = false;
+            }
+        }
+    }
+
     private void FixedUpdate()
     {
-        rb.AddForce(transform.up * moveAmount);
-
-        rb.AddTorque(rotateAmount);
-
-        if (Input.GetKey(KeyCode.Space) && playerNum == 1)
+        if (isStunned == false)
         {
-
+            rb.AddForce(transform.up * moveAmount);
         }
+        
+        rb.AddTorque(rotateAmount);
     }
 
     public int GetPlayerNumber()
     {
         return playerNum;
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Back"))
+        {
+            isStunned = true;
+            stunnedTimer = 0;
+        }
+    }
+
+
+
 }
